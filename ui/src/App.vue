3738c8c4b75e208fb8146b00f97b4d92fa6d29bb
cumathka <cumakaya2531@gmail.com>
2025-08-03 22:47:20 +0200

    circle added

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,24 +1,55 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-white">
+  <div class="landing-page relative overflow-x-hidden">
+    <!-- Daireler -->
+    <DecorativeCircle
+      :size="400"
+      color="rgba(255, 243, 205, 1)"
+      top="40%"
+      left="-250px"
+    />
+    <DecorativeCircle
+      :size="350"
+      color="rgba(255, 243, 205, 1)"
+      top="55%"
+      right="-200px"
+    />
+    <DecorativeCircle
+      :size="250"
+      color="rgba(255, 229, 208, 1)"
+      top="67%"
+      left="-150px"
+    />
+    <DecorativeCircle
+      :size="475"
+      color="rgba(255, 229, 208, 1)"
+      top="32%"
+      right="-300px"
+    />
+
     <TheNavbar />
-    <main class="flex-1">
-      <router-view />
-    </main>
+    <div>
+      <router-view v-if="$route.path !== '/'" />
+      <HomeView v-else />
+    </div>
     <BaseImage />
     <FooterSection />
   </div>
 </template>
 
 <script>
+import DecorativeCircle from '@/components/DecorativeCircle.vue'
 import TheNavbar from '@/components/TheNavbar.vue'
 import FooterSection from '@/components/FooterSection.vue'
+import HomeView from '@/views/HomeView.vue'
 import BaseImage from '@/components/BaseImage.vue'
 
 export default {
   name: 'App',
   components: {
+    DecorativeCircle,
     TheNavbar,
     FooterSection,
+    HomeView,
     BaseImage
   }
 }
---
+++ ui/src/components/DecorativeCircle.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    :style="{
+      position: 'absolute',
+      width: size + 'px',
+      height: size + 'px',
+      backgroundColor: color,
+      borderRadius: '50%',
+      top,
+      left,
+      right,
+      bottom,
+      zIndex: 0,
+      opacity
+    }"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "DecorativeCircle",
+  props: {
+    size: { type: [Number, String], default: 120 },
+    color: { type: String, default: 'red' },
+    top: { type: String, default: null },
+    left: { type: String, default: null },
+    right: { type: String, default: null },
+    bottom: { type: String, default: null },
+    opacity: { type: Number, default: 1 }
+  }
+}
+</script>
